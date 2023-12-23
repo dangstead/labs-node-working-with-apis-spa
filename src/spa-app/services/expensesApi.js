a6dcdbff9e62d8c777bff22420ad7a3516863937
Dan Angstead <dan.angstead@icsynergy.com>
2023-12-23 13:34:52 -0600

    Update expenses service to call API

--- src/spa-app/services/expensesApi.js
+++ src/spa-app/services/expensesApi.js
@@ -18,18 +18,16 @@ const expensesApi = {
   getReports: async () => {
     try {
       // Replace this with call to API
-      return [
-        {
-          date: new Date(),
-          description: "Pizza for a Coding Dojo session.",
-          value: 102,
+      const token = await window.auth0Client.getTokenSilently();
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-          date: new Date(),
-          description: "Coffee for a Coding Dojo session.",
-          value: 42,
-        },
-      ];
+      };
+      const res = await fetch(`${window.env.API_URL}/reports`, options);
+      const json = await res.json();
+      return json;
       // Replace this with call to API
     } catch (err) {
       console.log("Error getting reports", err);
